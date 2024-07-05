c7be925c8fa09fd4ac8d083e4323af4f2369afc0
Nguyễn Thị Lan <nguyenthilan02102003@gmail.com>
2024-07-05 10:26:50 +0700

    Update components.js

--- components.js
+++ components.js
@@ -7,12 +7,15 @@ document.querySelectorAll('.accordion-header').forEach(header => {
             document.querySelectorAll('.accordion-content').forEach(el => {
                 content.previousElementSibling.querySelector('.toggle-icon').src = './assets/images/play3.svg';
                 el.style.display = 'none';
+                this.style.borderRadius = '5px';
             });
             content.style.display = 'block';
             icon.src = './assets/images/down.svg';
+            this.style.borderRadius = '5px 5px 0 0';
         } else {
             content.style.display = 'none';
             icon.src = './assets/images/play3.svg';
+            this.style.borderRadius = '5px';
 
         }
     });
