2ce76ea320b685c91d0d2a5987d146e81dede6c0
Nguyễn Thị Lan <nguyenthilan02102003@gmail.com>
2024-07-05 10:20:19 +0700

    Update components.js

--- components.js
+++ components.js
@@ -1,21 +1,20 @@
+
 document.querySelectorAll('.accordion-header').forEach(header => {
     header.addEventListener('click', function () {
         const icon = this.querySelector('.toggle-icon');
-        const itemContent = header.nextElementSibling;
-
-        document.querySelectorAll('.accordion-content').forEach(content => {
-            if (content !== itemContent) {
-                content.style.display = 'none';
-                content.previousElementSibling.querySelector('.toggle-icon').src = './assets/images/play3.svg'; 
-            }
-        });
-
-        if (window.getComputedStyle(itemContent).display === 'none') {
-            itemContent.style.display = 'block';
+        const content = this.nextElementSibling;
+        if (content.style.display === 'none' || !content.style.display) {
+            document.querySelectorAll('.accordion-content').forEach(el => {
+                content.previousElementSibling.querySelector('.toggle-icon').src = './assets/images/play3.svg';
+                el.style.display = 'none';
+            });
+            content.style.display = 'block';
             icon.src = './assets/images/down.svg';
         } else {
-            itemContent.style.display = 'none';
+            content.style.display = 'none';
             icon.src = './assets/images/play3.svg';
+
         }
     });
 });
+
