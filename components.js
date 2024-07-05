9f950fc47d0a8204dbeb4add7f1a86bd5abc0828
Nguyễn Thị Lan <nguyenthilan02102003@gmail.com>
2024-07-05 15:30:50 +0700

    Update components.js

--- components.js
+++ components.js
@@ -1,25 +1,25 @@
 
-document.querySelectorAll('.accordion-header').forEach(header => {
-    header.addEventListener('click', function () {
-        const content = this.nextElementSibling;
-        const icon = this.querySelector('.toggle-icon');
-        if (content.style.display === 'none' || !content.style.display) {
-            document.querySelectorAll('.accordion-content').forEach(el => {
-                content.previousElementSibling.querySelector('.toggle-icon').src = './assets/images/play3.svg';
-                el.style.display = 'none';
-                this.style.borderRadius = '5px';
-            });
-            content.style.display = 'block';
-            icon.src = './assets/images/down.svg';
-            this.style.borderRadius = '5px 5px 0 0';
-        } 
-        else {
-            content.style.display = 'none';
-            icon.src = './assets/images/play3.svg';
+var header = document.getElementsByClassName("accordion-header");
+var i;
+for (i = 0; i < header.length; i++) {
+    header[i].addEventListener('click', function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    const icon = this.querySelector('.toggle-icon');
+    if (content.style.maxHeight===null || !content.style.maxHeight) {
+        document.querySelectorAll('.accordion-content').forEach(el => {
+            content.previousElementSibling.querySelector('.toggle-icon').src = './assets/images/play3.svg';
+            el.style.maxHeight = null;
             this.style.borderRadius = '5px';
-        }
-    });
-});
-
-
-
+        });
+        content.style.maxHeight =content.scrollHeight + "px";
+        icon.src = './assets/images/down.svg';
+        this.style.borderRadius = '5px 5px 0 0';
+    } 
+    else {
+        content.style.maxHeight = null;
+        icon.src = './assets/images/play3.svg';
+        this.style.borderRadius = '5px';
+    }
+ });
+}
