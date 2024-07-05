b9f3c395f800d8bc62fb0dfa42261b98ef4ba8ae
Nguyễn Thị Lan <nguyenthilan02102003@gmail.com>
2024-07-05 15:54:55 +0700

    Update components.js

--- components.js
+++ components.js
@@ -1,25 +1,31 @@
 
-var header = document.getElementsByClassName("accordion-header");
-var i;
-for (i = 0; i < header.length; i++) {
-    header[i].addEventListener('click', function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    const icon = this.querySelector('.toggle-icon');
-    if (content.style.maxHeight===null || !content.style.maxHeight) {
-        document.querySelectorAll('.accordion-content').forEach(el => {
-            content.previousElementSibling.querySelector('.toggle-icon').src = './assets/images/play3.svg';
-            el.style.maxHeight = null;
-            this.style.borderRadius = '5px';
+var headers = document.getElementsByClassName("accordion-header");
+for (var i = 0; i < headers.length; i++) {
+    headers[i].addEventListener('click', function () {
+        var isActive = this.classList.contains("active");
+        var content = this.nextElementSibling;
+        var icon = this.querySelector('.toggle-icon');
+
+
+        Array.from(headers).forEach(function (header) {
+            var otherContent = header.nextElementSibling;
+            var otherIcon = header.querySelector('.toggle-icon');
+            otherContent.style.maxHeight = null;
+            otherIcon.src = './assets/images/play3.svg';
+            header.style.borderRadius = '5px';
+            header.classList.remove('active');
         });
-        content.style.maxHeight =content.scrollHeight + "px";
-        icon.src = './assets/images/down.svg';
-        this.style.borderRadius = '5px 5px 0 0';
-    } 
-    else {
-        content.style.maxHeight = null;
-        icon.src = './assets/images/play3.svg';
-        this.style.borderRadius = '5px';
-    }
- });
+
+        if (!isActive) {
+            this.classList.add("active");
+            content.style.maxHeight = content.scrollHeight + "px";
+            icon.src = './assets/images/down.svg';
+            this.style.borderRadius = '5px 5px 0 0';
+        } else {
+            this.classList.remove("active");
+            content.style.maxHeight = null;
+            icon.src = './assets/images/play3.svg';
+            this.style.borderRadius = '5px';
+        }
+    });
 }
